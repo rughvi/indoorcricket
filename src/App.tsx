afaf6dfd80fd6d5237f78d731ebd1aeac52ff46b
rughvi <rughvi8784@gmail.com>
2026-02-08 23:54:04 +0000

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Main from './Main';
+import GameSelection from './Game/GameSelection';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Main}></Route>
+        <Route path="/gameSelection" Component={GameSelection}></Route>    
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
