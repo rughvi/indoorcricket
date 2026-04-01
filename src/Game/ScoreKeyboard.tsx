153cbea512a3a9ff49c0b26b266007c3f459a2c9
rughvi <rughvi8784@gmail.com>
2026-04-01 22:03:12 +0100

    score selected overlay

--- src/Game/ScoreKeyboard.tsx
+++ src/Game/ScoreKeyboard.tsx
@@ -1,30 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import '../CSS/ScoreKeyboard.css';
 import { ScoreKeyEvent, ScoreKeyEventType } from "../Models/ScoreKeyEvent";
 
 const ScoreKeyboard = (props: {onClick: (keyPressed: ScoreKeyEventType) => void }) => {
+    const [isAnimate, setIsAnimate] = useState<boolean>(false);
+    const [scorekey, setScoreKey] = useState<ScoreKeyEventType>(ScoreKeyEvent.Dot);
+    const onClickScoreKey = (keyPressed: ScoreKeyEventType) => {
+        setScoreKey(keyPressed);
+        setIsAnimate(true);
+        props.onClick(keyPressed);
+        setTimeout(() => {
+            setScoreKey(ScoreKeyEvent.Dot);
+            setIsAnimate(false);
+        }, 500);
+    };
+
     return (
-        <div className="ScoreKeyboardCard">
+        <div className={isAnimate? 'ScoreKeyboardCard': 'ScoreKeyboardCard'}>
+            {isAnimate && <div className={isAnimate? 'ScoreKeyboardCardOverlayShow': 'ScoreKeyboardCardOverlay'}>{scorekey.label}</div>}
             <div className="ScoreKeyboardRow">
-                <div className="ScoreKeyboardElement" onClick={() => props.onClick(ScoreKeyEvent.One)}>1</div>
-                <div className="ScoreKeyboardElement" onClick={() => props.onClick(ScoreKeyEvent.Two)}>2</div>
-                <div className="ScoreKeyboardElement" onClick={() => props.onClick(ScoreKeyEvent.Three)}>3</div>
-                <div className="ScoreKeyboardElement" onClick={() => props.onClick(ScoreKeyEvent.Four)}>4</div>
-                <div className="ScoreKeyboardElement" onClick={() => props.onClick(ScoreKeyEvent.Six)}>6</div>
+                <div className="ScoreKeyboardElement" onClick={() => onClickScoreKey(ScoreKeyEvent.One)}>1</div>
+                <div className="ScoreKeyboardElement" onClick={() => onClickScoreKey(ScoreKeyEvent.Two)}>2</div>
+                <div className="ScoreKeyboardElement" onClick={() => onClickScoreKey(ScoreKeyEvent.Three)}>3</div>
+                <div className="ScoreKeyboardElement" onClick={() => onClickScoreKey(ScoreKeyEvent.Four)}>4</div>
+                <div className="ScoreKeyboardElement" onClick={() => onClickScoreKey(ScoreKeyEvent.Six)}>6</div>
             </div>
             <br />
             <div className="ScoreKeyboardRow">                
-                <div className="ScoreKeyboardElement" onClick={() => props.onClick(ScoreKeyEvent.NoBall)}>NB</div>
-                <div className="ScoreKeyboardElement" onClick={() => props.onClick(ScoreKeyEvent.NoBallPlusOne)}>NB+1</div>
-                <div className="ScoreKeyboardElement" onClick={() => props.onClick(ScoreKeyEvent.NoBallPlusTwo)}>NB+2</div>
-                <div className="ScoreKeyboardElement" onClick={() => props.onClick(ScoreKeyEvent.NoBallPlusFour)}>NB+4</div>
-                <div className="ScoreKeyboardElement" onClick={() => props.onClick(ScoreKeyEvent.NoBallPlusSix)}>NB+6</div>
+                <div className="ScoreKeyboardElement" onClick={() => onClickScoreKey(ScoreKeyEvent.NoBall)}>NB</div>
+                <div className="ScoreKeyboardElement" onClick={() => onClickScoreKey(ScoreKeyEvent.NoBallPlusOne)}>NB+1</div>
+                <div className="ScoreKeyboardElement" onClick={() => onClickScoreKey(ScoreKeyEvent.NoBallPlusTwo)}>NB+2</div>
+                <div className="ScoreKeyboardElement" onClick={() => onClickScoreKey(ScoreKeyEvent.NoBallPlusFour)}>NB+4</div>
+                <div className="ScoreKeyboardElement" onClick={() => onClickScoreKey(ScoreKeyEvent.NoBallPlusSix)}>NB+6</div>
             </div>
             <br />
             <div className="ScoreKeyboardRow">
-                <div className="ScoreKeyboardElement" onClick={() => props.onClick(ScoreKeyEvent.Wide)}>WD</div>
-                <div className="ScoreKeyboardElement" onClick={() => props.onClick(ScoreKeyEvent.Dot)}>0</div>
-                <div className="ScoreKeyboardElement" style={{ backgroundColor: "#c31212" }} onClick={() => props.onClick(ScoreKeyEvent.Wicket)}>WK</div>
+                <div className="ScoreKeyboardElement" onClick={() => onClickScoreKey(ScoreKeyEvent.Wide)}>WD</div>
+                <div className="ScoreKeyboardElement" onClick={() => onClickScoreKey(ScoreKeyEvent.Dot)}>0</div>
+                <div className="ScoreKeyboardElement" style={{ backgroundColor: "#c31212" }} onClick={() => onClickScoreKey(ScoreKeyEvent.Wicket)}>WK</div>
             </div>
         </div>
     );
