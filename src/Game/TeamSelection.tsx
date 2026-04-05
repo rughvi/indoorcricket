c62fc52547ec56875e4a34e03616a4682a6b0185
rughvi <rughvi8784@gmail.com>
2026-04-05 11:22:39 +0100

    Back navigation in team selection

--- src/Game/TeamSelection.tsx
+++ src/Game/TeamSelection.tsx
@@ -7,6 +7,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { IRootState } from "../store/store";
 import { assignPlayersToTeams } from "../store/slices/gameSlice";
 import { useNavigate, useParams } from "react-router-dom";
+import { ReactComponent as Back } from '../back.svg';
 
 const TeamSelection= () => {
     const { teamIndex } = useParams();
@@ -30,18 +31,26 @@ const TeamSelection= () => {
     };
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>Select players for Team 1</p>
-                    <ul className="TeamSelectionUL">
-                        {selectablePlayers.map((player, index) => (
-                            <li key={index}>
-                                <button className={`PlayerButton ${player.selected === true? 'ButtonSelected' : 'Button'}`} onClick={() => { selectPlayer(player, index) }}> {player.name} </button>
-                            </li>
-                        ))}
-                    </ul>
+        <div className="Form">
+            <div className="GameCard">
+                <div className="GameCard-header">
+                    <Back style={{width: "30px", height:"30px"}} onClick={() => {navigate('/gameSelection')}}></Back>
+                    Select players for Team {teamIndex}
+                    <div style={{width: "30px"}}></div>
+                </div>
+            </div>
+            <div className="GameCard">
+                <ul className="TeamSelectionUL">
+                    {selectablePlayers.map((player, index) => (
+                        <li key={index}>
+                            <button className={`PlayerButton ${player.selected === true? 'ButtonSelected' : 'Button'}`} onClick={() => { selectPlayer(player, index) }}> {player.name} </button>
+                        </li>
+                    ))}
+                </ul>                
+            </div>
+            <div className="GameCard">
                 <button className="ActionButton" onClick={() => { onPlayerSelectionDone()}}>Ok</button>
-            </header>
+            </div>
         </div>
     );
 };
